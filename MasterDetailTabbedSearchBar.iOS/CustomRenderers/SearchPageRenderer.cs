235494014ffacf01b833dbe69efbe7e051372ca6
Brandon Minnick <13558917+brminnick@users.noreply.github.com>
2019-10-07 12:59:14 -0500

    First Commit

---
+++ MasterDetailTabbedSearchBar.iOS/CustomRenderers/SearchPageRenderer.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UIKit;
+using MasterDetailTabbedSearchBar;
+using MasterDetailTabbedSearchBar.iOS;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.iOS;
+
+[assembly: ExportRenderer(typeof(MySearchPage), typeof(SearchPageRenderer))]
+namespace MasterDetailTabbedSearchBar.iOS
+{
+    public class SearchPageRenderer : PageRenderer, IUISearchResultsUpdating
+    {
+        bool _isFirstAppearing = true;
+
+        public override void WillMoveToParentViewController(UIViewController parent)
+        {
+            base.WillMoveToParentViewController(parent);
+
+            var searchController = new UISearchController(searchResultsController: null)
+            {
+                SearchResultsUpdater = this,
+                DimsBackgroundDuringPresentation = false,
+                HidesNavigationBarDuringPresentation = false,
+                HidesBottomBarWhenPushed = true
+            };
+            searchController.SearchBar.Placeholder = string.Empty;
+
+            parent.NavigationItem.SearchController = searchController;
+
+            DefinesPresentationContext = true;
+        }
+
+        public override void ViewDidAppear(bool animated)
+        {
+            base.ViewDidAppear(animated);
+
+            //Work-around to ensure the SearchController appears when the page first appears https://stackoverflow.com/a/46313164/5953643
+            if (_isFirstAppearing)
+            {
+                ParentViewController.NavigationItem.SearchController.Active = true;
+                ParentViewController.NavigationItem.SearchController.Active = false;
+
+                _isFirstAppearing = false;
+            }
+        }
+
+        public void UpdateSearchResultsForSearchController(UISearchController searchController)
+        {
+            if (Element is ISearchPage searchPage)
+                searchPage.OnSearchBarTextChanged(searchController.SearchBar.Text);
+        }
+    }
+}
---
+++ MasterDetailTabbedSearchBar/App.cs
@@ -0,0 +1,71 @@
+﻿using System;
+
+using Xamarin.Forms;
+using Xamarin.Forms.PlatformConfiguration;
+using Xamarin.Forms.PlatformConfiguration.iOSSpecific;
+using Xamarin.Forms.PlatformConfiguration.AndroidSpecific;
+
+namespace MasterDetailTabbedSearchBar
+{
+    public class App : Xamarin.Forms.Application
+    {
+        public App()
+        {
+            var navigationPage = new Xamarin.Forms.NavigationPage(new MySearchPage())
+            {
+                Title = "Tab 1",
+                BarTextColor = Color.Orange
+            };
+            navigationPage.On<iOS>().SetPrefersLargeTitles(true);
+
+            var tabbedPage = new Xamarin.Forms.TabbedPage
+            {
+                Children =
+                {
+                    navigationPage,
+                    new ContentPage{ Title = "Tab 2" },
+                    new ContentPage{ Title = "Tab 3" }
+                }
+            };
+            tabbedPage.On<Android>().SetToolbarPlacement(ToolbarPlacement.Bottom);
+
+            MainPage = new MasterDetailPage
+            {
+                Master = new ContentPage
+                {
+                    Title = "☰",
+                    BackgroundColor = Color.LightGray
+                },
+                Detail = tabbedPage
+            };
+        }
+    }
+
+    //how about adding a searchbar on a masterdetail page, with a tabbed page inside and showing
+    //the search bar only on 1 of the pages inside the tabbedpage? got this working on Android, but no luk yet with iOS
+
+    public class MySearchPage : ContentPage, ISearchPage
+    {
+        public MySearchPage()
+        {
+            Title = "Search Page";
+            SearchBarTextChanged += HandleSearchBarTextChanged;
+        }
+
+        public event EventHandler<string> SearchBarTextChanged;
+
+        public void OnSearchBarTextChanged(string text) => SearchBarTextChanged?.Invoke(this, text);
+
+        void HandleSearchBarTextChanged(object sender, string searchBarText)
+        {
+            //Logic to handle updated search bar text
+        }
+    }
+
+    public interface ISearchPage
+    {
+        void OnSearchBarTextChanged(string text);
+        event EventHandler<string> SearchBarTextChanged;
+    }
+}
+
